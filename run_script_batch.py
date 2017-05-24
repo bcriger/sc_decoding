89e5c459dada69dbe48f9ff950ed3786e47e4702
Ben Criger <BCriger@GMail.com>
2017-05-24 15:14:00 +0200

    minor changes to run_script_batch.py

--- run_script_batch.py
+++ run_script_batch.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     sim_type = argv[7] if len(argv) > 7 else 'iidxz'
     start_dx, end_dx = map(int,argv[8:10])
 
-    str_dists = "'"+argv[4]+"'"
+    str_dists = "'" + argv[4] + "'"
 
-    for job_num in range(start_dx, end_dx+1):
+    for job_num in range(start_dx, end_dx + 1):
         new_argv = ["python", "run_script.py"] + argv[1:4] + [str_dists] + [argv[5]] + [flnm + '_' + str(job_num) + ".pkl"] + [sim_type] + [" &"]
         print ' '.join(new_argv)
         os.system(' '.join(new_argv))
